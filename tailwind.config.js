de2f4d8980efa49866185c02a6ae8d42f5d47c1f
samir <samirgm.dev@gmail.com>
2024-03-12 16:21:05 -0500

    ci: :wrench: add development configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        grayLigthCard: '#212936cc',
+        grayDarkCard: '#121826cc',
+        grayDark: '#4d5562',
+        grayDarktext: '#394150',
+        grayLightext: '#cdd5e0',
+        whiteText: '#f9fafb',
+        lightText: '#f9fafb',
+        blueLight: '#7ca9f3',
+        blueDark: '#3662e3',
+        backgraundContain: '#040711'
+      },
+    },
   },
   plugins: [],
 }
